6f963d95550e9552212c9ffcb167aaef9e6fb095
PepeLira <jflira@miuandes.cl>
2023-04-27 00:46:18 -0400

    feat(dataManagement): addgetGameObject

--- js/dataManagement.js
+++ js/dataManagement.js
@@ -13,7 +13,16 @@ export function renewTokens(data){
 export function getGameObjects(data){
     let gameObjects = [];
     for (let i = 0; i < data.length; i++) {
-        gameObjects.push(new Game(data[i].id, data[i].name, data[i].question_time, data[i].answer_time, data[i].status, data[i].players, data[i].questions));
+        gameObjects.push(new Game(data[i].id,data[i].name, data[i].creator, data[i].created, data[i].player_count, data[i].players, data[i].i_can_start, data[i].question_time, data[i].answer_time, data[i].rounds_number));
     }
     return gameObjects;
+}
+
+export function getGameObject(data, id){
+    for (let i = 0; i < data.length; i++) {
+        if(data[i].id == id){
+            return new Game(data[i].id,data[i].name, data[i].creator, data[i].created, data[i].player_count, data[i].players, data[i].i_can_start, data[i].question_time, data[i].answer_time, data[i].rounds_number);
+        }
+    }
+    return "not found";
 }
