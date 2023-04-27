ba0b61f6de2e0fae4b7d410671aef7d01f63f3ee
nccaster <ncapara@miuandes.cl>
2023-04-27 00:45:27 -0400

    feat: games list cloning

--- js/views/gamesIndex.js
+++ js/views/gamesIndex.js
@@ -15,7 +15,46 @@ function closeModal() {
 var gamesList = [];
 
 function renderGames(games) {
+	console.log(Object.keys(games).length);
 	console.log(games);
+	let parent = document.getElementById("roomsContainer");
+	let elem = document.getElementsByClassName("template")[0];
+	for (let index = 0; index < Object.keys(games).length; index++) {
+		//clone the elem
+		let clonedElement = elem.cloneNode(true);
+		clonedElement.id = games[index].id;
+		clonedElement.classList.remove("template");
+		//get first child
+		let titlediv = clonedElement.getElementsByClassName("room-players")[0];
+		titlediv.textContent = games[index].name;
+		//<div class="flex-row player-tag player"></div>  puede ser player o pregunton la clase
+		//el class "icon" src puede ser src="css/player.svg"  o src="css/Pregunton.svg"
+		//get player list
+		let pListContainer = clonedElement.getElementsByClassName("players-tags-container")[0];
+		let pList = clonedElement.getElementsByClassName("player-tag")[0];
+		let pTagName = clonedElement.getElementsByClassName("player-tag-name")[0];
+		for (let player = 0; player < Object.keys(games[index].players).length; player++) {	
+			if (games[index].players[player].id !== games[index].creator.id) {
+				let plistClone = pList.cloneNode(true);
+				let icon = plistClone.getElementsByClassName("icon")[0];
+				let tName = plistClone.getElementsByClassName("player-tag-name")[0];
+				plistClone.classList.remove("pregunton");
+				plistClone.classList.add("player");
+				icon.src = "css/player.svg";
+				tName.textContent = games[index].players[player].username;
+				pListContainer.appendChild(plistClone);
+			}
+			else {
+				pTagName.textContent = games[index].creator.username;
+			}
+		}
+
+
+		//append to parent div
+		parent.appendChild(clonedElement);
+	}
+	console.log(elem);
+	console.log(clonedElement);
 }
 
 gameList(localStorage.getItem("access"), renderGames);
